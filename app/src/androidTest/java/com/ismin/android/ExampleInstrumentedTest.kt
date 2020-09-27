64a3109ab6981450e7ba7c4f3a38fc0b5b8ebc76
Gaëtan Maisse <gmaisse@solocal.com>
2020-09-27 12:02:47 +0200

    feat: add README and tests

--- app/src/androidTest/java/com/ismin/android/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ismin/android/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ismin.androidtp2
+package com.ismin.android
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ismin.androidtp2", appContext.packageName)
+        assertEquals("com.ismin.android", appContext.packageName)
     }
 }
